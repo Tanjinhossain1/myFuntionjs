03ad09c9da7fa988ca0e1e5806c399fb02fa2e8e
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-02-11 15:44:04 +0600

    of

--- baperbank.js
+++ baperbank.js
@@ -3,17 +3,27 @@ function double(){
 const doubleInput = document.getElementById('double-input')
 const doubleInputValue = doubleInput.value;
 const doubleValueConvert = parseFloat(doubleInputValue)
-const doubleNumbers = doubleValueConvert *  2;
-const numberBox = document.getElementById('number-box');
-numberBox.innerText = doubleNumbers;
+if(doubleValueConvert > 0){
+    const doubleNumbers = doubleValueConvert *  2;
+    const numberBox = document.getElementById('number-box');
+    numberBox.innerText = doubleNumbers;
+}
+else{
+    alert('Not Allow')
+}
 doubleInput.value = '';
 }
 function triple(){
     const tripleInput = document.getElementById('double-input')
     const tripleInputValue = tripleInput.value;
     const triplaValueConvert = parseFloat(tripleInputValue)
-    const tripleNumbers = triplaValueConvert *  3;
-    const numberBox = document.getElementById('number-box');
-    numberBox.innerText = tripleNumbers;
+    if(triplaValueConvert > 0){
+        const tripleNumbers = triplaValueConvert *  3;
+        const numberBox = document.getElementById('number-box');
+        numberBox.innerText = tripleNumbers;
+    }
+    else{
+        alert('Not Allow')
+    }
     tripleInput.value = '';
 }
